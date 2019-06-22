ce3580a42c619ec484f38084bd781f4170ca9b94
Jeremy Long <jeremy.long@gmail.com>
2019-06-22 07:41:33 -0400

    make test case more robust - so they do not fail when central is down

--- core/src/test/java/org/owasp/dependencycheck/data/central/CentralSearchTest.java
+++ core/src/test/java/org/owasp/dependencycheck/data/central/CentralSearchTest.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Assume;
+import org.owasp.dependencycheck.utils.Settings;
 
 /**
  * Created by colezlaw on 10/13/14.
@@ -42,10 +43,16 @@ public class CentralSearchTest extends BaseTest {
     // test it anyway
     @Test
     public void testValidSha1() throws Exception {
+        try {
         List<MavenArtifact> ma = searcher.searchSha1("9977a8d04e75609cf01badc4eb6a9c7198c4c5ea");
         assertEquals("Incorrect group", "org.apache.maven.plugins", ma.get(0).getGroupId());
         assertEquals("Incorrect artifact", "maven-compiler-plugin", ma.get(0).getArtifactId());
         assertEquals("Incorrect version", "3.1", ma.get(0).getVersion());
+                } catch (IOException ex) {
+            //we hit a failure state on the CI
+            Assume.assumeFalse(StringUtils.contains(ex.getMessage(), "Gateway Time-out"));
+            throw ex;
+        }
     }
 
     // This test does generate network traffic and communicates with a host
@@ -53,7 +60,13 @@ public class CentralSearchTest extends BaseTest {
     // test it anyway
     @Test(expected = IOException.class)
     public void testMissingSha1() throws Exception {
+        try {
         searcher.searchSha1("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                } catch (IOException ex) {
+            //we hit a failure state on the CI
+            Assume.assumeFalse(StringUtils.contains(ex.getMessage(), "Gateway Time-out"));
+            throw ex;
+        }
     }
 
     // This test should give us multiple results back from Central
